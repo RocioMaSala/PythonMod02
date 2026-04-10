759aee4bb13f5e9a9da93310caba9c965efa7d25
RocioMaSala <martinrocio16@gmail.com>
2026-04-10 13:52:41 +0200

    10.4

--- ex2/ft_different_errors.py
+++ ex2/ft_different_errors.py
@@ -1,5 +1,4 @@
-
-def garden_operations(operation_number: int):
+def garden_operations(operation_number: int) -> None:
     print(f"Testing operation {operation_number}...")
     if operation_number == 0:
         int("abc")
@@ -8,24 +7,26 @@ def garden_operations(operation_number: int):
     elif operation_number == 2:
         open("/non/existent/file")
     elif operation_number == 3:
-        len(5)
+        "hello" + 5
     else:
         print("Operation completed successfully")
 
-def test_error_types(): ## Me quedo aquí
-    for operation_number in range (0, 5):
+
+def test_error_types() -> None:
+    for operation_number in range(0, 5):
         try:
             garden_operations(operation_number)
-        except Exception as e:
-            if operation_number == 0: 
-                raise ValueError(f"Caught ValueError: {e}") from e
-            elif operation_number == 1: 
-                raise ZeroDivisionError(f"Caught ZeroDivisionError: {e}") from e
-            elif operation_number == 2: 
-                raise FileNotFoundError(f"Caught FileNotFoundError: {e}") from e
-            elif operation_number == 3: 
-                raise TypeError(f"Caught TypeError: {e}") from e
+        except ValueError as e:
+            print(f"Caught ValueError: {e}")
+        except ZeroDivisionError as e:
+            print(f"Caught ZeroDivisionError: {e}")
+        except FileNotFoundError as e:
+            print(f"Caught FileNotFoundError: {e}")
+        except TypeError as e:
+            print(f"Caught TypeError: {e}")
 
 
 if __name__ == "__main__":
     test_error_types()
+    print()
+    print("All error types tested successfully!")
---
+++ ex3/ft_custom_errors.py
@@ -0,0 +1,51 @@
+class GardenError (Exception):
+    def __init__(self, e: str = "Unknown Garden Error") -> None:
+        super().__init__(e)
+
+
+class PlantError(GardenError):
+    def __init__(self, e: str = "Unknown Plant Error") -> None:
+        super().__init__(e)
+
+
+class WaterError(GardenError):
+    def __init__(self, e: str = "Unknown Water Error") -> None:
+        super().__init__(e)
+
+
+def checkplant() -> None:
+
+    raise PlantError("The tomato plant is wilting!")
+
+
+def checkwater() -> None:
+    raise WaterError("Not enough water in the tank!")
+
+
+def test_error_types() -> None:
+    print("=== Custom Garden Errors Demo ===\n")
+    try:
+        print("Testing PlantError...")
+        checkplant()
+    except PlantError as e:
+        print(f"Caught PlantError:{e}\n")
+    try:
+        print("Testing WaterError...")
+        checkwater()
+    except WaterError as e:
+        print(f"Caught WaterError: {e}\n")
+    try:
+        print("Testing catching all garden errors...")
+        checkplant()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+    try:
+        checkwater()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+
+
+if __name__ == "__main__":
+    test_error_types()
+    print()
+    print("All error types tested successfully!")
